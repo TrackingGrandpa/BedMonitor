df564a1640a713a42dae962bab8ad4d6cfd8a525
Matthew Sberna <matthew.sberna@philips.com>
2016-02-26 14:44:27 -0500

    Adding response to no

---
+++ TestApplication/TestApplication/TestApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Does it do how we do? yes or no");
+            string yesorno = Console.ReadLine();
+            if (yesorno == "yes")
+            {
+                Console.WriteLine("yay!");
+            }
+            else if (yesorno == "no")
+                Console.WriteLine("why not?");
+
+            else
+                Console.WriteLine("awwww :(");
+
+            Console.Write("Press enter to continue");
+            Console.Read();
+        }
+    }
+}
